45ce027400230c259e8f3a5c3f13945ab6d4bfc0
NamitsGit <trail99blazer@gmail.com>
2023-07-17 10:33:05 +0530

    Added jinja templating and made changes in routes

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ with app.app_context():
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
     if request.method == 'POST':
-        # print("POST method detected!")
         title = request.form['title']
         description = request.form['description']
         todo = Todo(title=title, description=description)
@@ -34,7 +33,6 @@ def hello_world():
     allTodo = Todo.query.all()
     print(allTodo)  
     return render_template("index.html", allTodo=allTodo)
-    # return render_template("index.html")
 
 @app.route('/home')
 def home_hello_world():
@@ -45,11 +43,9 @@ def show_todos():
     allTodo = Todo.query.all()
     print(allTodo)  
     return render_template("index.html", allTodo=allTodo)
-    # return "This is the show page"
 
 @app.route('/about')
 def about():
-    # return 'Hello, World!'
     return render_template("about.html")
 
 @app.route('/contact')
@@ -73,12 +69,9 @@ def update(sno):
         return redirect("/")
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template("update.html", todo=todo)
-    # return 'Hello, World!'
-    # return render_template("about.html")
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    # return 'Hello, World!'
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
